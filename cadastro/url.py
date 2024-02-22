e01e9b8d536ec3fd2bb76fd12232c946a80fea1a
Alex <allexprogram@gmail.com>
2024-02-22 12:21:17 -0300

    Correçõe incluindo o módulo deleta

--- cadastro/url.py
+++ cadastro/url.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('update-aluno/', view_update_aluno, name='update-aluno'),
     path('update-estoque/',view_update_estoque, name='update-estoque'),
     path('update-vendas/', view_update_vendas, name='update-vendas'),
-    # path('update-aluno/', update_debito_model_aluno, name='update-aluno'), renomear este caminho.
+    path('update-aluno/', update_debito_model_aluno, name='update-aluno'), #renomear este caminho.
     path('update-produto/', update_debito_model_produto, name='update-produto'),
     
     path('lista-vendas/',view_lista_vendas, name='lista-vendas'),
@@ -48,7 +48,7 @@ urlpatterns = [
     path('pesquisa/',view_pesquisa, name='pesquisa'),
         
     path('delete-registro-debito/',delete_lista_debito, name='delete-registro-debito'),
-    path('delete-produto/',view_delete_produto, name='delete-produto'),
+     path('delete-produto/',view_delete_produto, name='delete-produto'),
 
 ]
 
--- carden/settings.py
+++ carden/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "upadate",
     "deleta",
 
+    
+
 ]
 
 MIDDLEWARE = [
