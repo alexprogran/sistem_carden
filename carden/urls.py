bc981a47cdaf1fe99bdad7099678beabeb7b0970
Alex <alexprogran@gmail.com>
2025-05-29 19:42:57 -0300

    Containerizando

--- carden/urls.py
+++ carden/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.shortcuts import redirect
+from cadastro.views import view_login
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 
@@ -10,6 +11,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("api-auth/",include('rest_framework.urls')), 
     # path("api/token/",TokenObtainPairView.as_view(),name="token_obtain_pair"),
+    path('',view_login, name='login' ),
     path("admin/", admin.site.urls),
     path('carden/', lambda request: redirect('cadastro:login'), name='home'),
     path('carden/', include('cadastro.url', namespace='cadastro')),
